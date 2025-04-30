e555eaf659c93a1e9c82034f2c145810b0d5c61d
KishimotoSatoruWem <satoru.kishimoto@vv-e-nn.com>
2025-04-30 18:12:09 +0900

    api url変更

--- src/api/MemoApi.tsx
+++ src/api/MemoApi.tsx
@@ -1,5 +1,8 @@
 import { MemoApiClient } from "../generated";
 
 // https://github.com/ferdikoomen/openapi-typescript-codegen/issues/477
-const memoApiClientOption = { HEADERS: { accept: "text/plain" } };
+const memoApiClientOption = { 
+    BASE: import.meta.env.VITE_API_BASE,
+    HEADERS: { accept: "text/plain" },
+ };
 export const memoApi = new MemoApiClient(memoApiClientOption);
